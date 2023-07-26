47a7e875b99dc4b30d90cc2809835740add1eb4f
GerardoFernandez7 <101151869+GerardoFernandez7@users.noreply.github.com>
2023-07-25 22:38:19 -0400

    Commit 5: Nuevos limites para el nuevo mapa

--- SpeedRacer/player1.java
+++ SpeedRacer/player1.java
@@ -2,9 +2,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class player1 here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Esta clase define lo que puede hacer el jugador y lo que no puede hacer
+ * @author (Gerardo Fernandez) 
+ * @version (7/25/23)
  */
 public class player1 extends Actor
 {
@@ -20,15 +20,15 @@ public class player1 extends Actor
     public void act()
     {
         if (Greenfoot.isKeyDown("up")){
-            if (getY() > 140)
+            if (getY() > 280)
             setLocation(getX(),getY()- velocidad);
         }
         if (Greenfoot.isKeyDown("down")){
-            if (getY() > 50)
+            if (getY() < 425)
             setLocation(getX(),getY()+ velocidad);
         }
         if (Greenfoot.isKeyDown("right")){
-            if (getX() < 450)
+            if (getX() < 950)
             setLocation(getX()+ velocidad,getY());
         }
         if (Greenfoot.isKeyDown("left")){
